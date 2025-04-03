4303e9ecb52e676dd3119786b1a485f6366960cf
Vegard M. <vm932648@gmail.com>
2025-04-03 15:22:35 +0200

    Update createBooking.ts

--- src/api/post/createBooking.ts
+++ src/api/post/createBooking.ts
@@ -4,10 +4,5 @@ import { MakeBooking } from "../../components/interfaces/bookingInterFace";
 
 export const createBooking = async (formInfo: MakeBooking) => {
   const bookingUrl = `${holidazeUrl}/bookings`;
-  console.log(bookingUrl);
-  console.log(formInfo);
-  console.log(typeof (formInfo.guests));
-
-
   await postData(bookingUrl, formInfo);
 };
